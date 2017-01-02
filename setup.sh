64e2b6553d1a638bec1e2974bd1cb13e7b1fded4
Moritz Hoffmann <moritz1@gmx.com>
2017-01-02 22:47:46 +0100

    Update download link

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ set -e
 # Get latest tarball and extract it
 cd /seafile
 
-wget "https://download.seafile.com/d/06d4ca0272/files/?p=/seafile-pro-server_${SEAFILE_VERSION}_x86-64.tar.gz&dl=1" -O "seafile-pro-server_${SEAFILE_VERSION}_x86-64.tar.gz"
+wget "https://download.seafile.com/d/6e5297246c/files/?p=/pro/seafile-pro-server_${SEAFILE_VERSION}_x86-64.tar.gz&dl=1" -O "seafile-pro-server_${SEAFILE_VERSION}_x86-64.tar.gz"
 tar -xzf "seafile-pro-server_${SEAFILE_VERSION}_x86-64.tar.gz"
 
 mkdir installed
